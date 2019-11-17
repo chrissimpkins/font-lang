d18570c61334ada4e188eacf0fd02b3b9b9140a1
Chris Simpkins <git.simpkins@gmail.com>
2019-11-16 23:17:13 -0500

    Add langs.swift

---
+++ font-lang/langs.swift
@@ -0,0 +1,12 @@
+//
+//  langs.swift
+//  font-lang
+//
+//  Created by Christopher Simpkins on 11/16/19.
+//  Copyright © 2019 Source Foundry. Apache License v2.0
+//
+
+let langDict = [
+    "aa": "Afar",
+    "aar": "Afar",
+]
